8e32a61242caeb0714e6857f8873faf42f0ccce7
White Tiger <dayley.zimmermann@gmail.com>
2022-02-18 16:03:29 -0500

    finished get and add notes

--- db/index.js
+++ db/index.js
@@ -1,20 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 
-function getNotes() {
+class NoteActions{
 
+  getNotes() {
+    return new Promise ((resolve, reject) => {
+      fs.readFile("db/db.json", "utf8", function (err, data) {
+        if(err) {
+          console.log(err);
+          resolve([])
+        }
+        resolve(JSON.parse(data));
+      })
+    })
+  };
+  
+  addNote (noteObj) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile('db/db.json', JSON.stringify(noteObj), function (err, data) {
+        if(err) {
+          console.log(err);
+          resolve('Failed to add note')
+        }
+        resolve("Success in adding new note");
+    })
+    });
+  };
+  // deleteNote(noteObj) {
+    //loop through each note object and check and make sure != id
+    //first read db (getnotes) then pass note object to be deleted (use filter method to check Ids of current notes)
+    //.filter(note => note.id != id)
+  // };
 };
-
-function addNote() {
-
-};
-
-function deleteNote() {
-
-};
-
-module.exports = {
-  getNotes,
-  addNote,
-  deleteNote
-};
+module.exports = new NoteActions();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
-const dbNotes = require('../db');
+const dbNotes = require('../db/index');
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
+const db = require('../db/index');
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 
@@ -15,19 +18,37 @@ router.get('/notes', (req, res) => {
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 router.post('/notes', (req, res) => {
+  const  {title, text} = req.body;
   dbNotes
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch((err) => res.status(500).json(err));
+    .getNotes()
+    .then((notes) => {
+      const parsedNotes = notes;
+      // console.log(notes);
+      const newNote = {
+        title: title,
+        text: text,
+        id: uuidv4()
+      }
+      // console.log(newNote)
+      parsedNotes.push(newNote);
+      // console.log(parsedNotes);
+      dbNotes.addNote(parsedNotes)
+      .then (result => {
+        console.log(res.json(result));
+      })
+    })
+    // .addNote(req.body)
+    // .then((note) => res.json(note))
+    // .catch((err) => res.status(500).json(err));
 });
 
-//DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+// //DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
-router.delete('/notes/:id', (req, res) => {
-  dbNotes
-    .deleteNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch((err) => res.status(500).json(err));
-});
+// router.delete('/notes/:id', (req, res) => {
+//   dbNotes
+//     .deleteNote(req.params.id)
+//     .then(() => res.json({ ok: true }))
+//     .catch((err) => res.status(500).json(err));
+// });
 
 module.exports = router;
